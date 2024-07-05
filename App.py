51b96a543f2523f907642e6bde728297586d11ab
nilashrathnr <148995142+nilashrathnr@users.noreply.github.com>
2024-07-05 16:11:49 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,460 @@
+from flask import Flask, render_template, flash, request, session
+
+import mysql.connector
+import sys
+
+app = Flask(__name__)
+app.config['DEBUG']
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+
+@app.route("/")
+def homepage():
+    return render_template('index.html')
+
+
+@app.route("/Home")
+def Home():
+    return render_template('index.html')
+
+
+@app.route("/AdminLogin")
+def DoctorLogin():
+    return render_template('AdminLogin.html')
+
+
+@app.route("/NewEmployee")
+def NewEmployee():
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM employeetb ")
+    data = cur.fetchall()
+    return render_template('NewEmployee.html', data=data)
+
+
+@app.route("/EmployeeLogin")
+def EmployeeLogin():
+    return render_template('EmployeeLogin.html')
+
+
+@app.route("/UserLogin")
+def UserLogin():
+    return render_template('UserLogin.html')
+
+
+@app.route("/NewUser")
+def NewUser():
+    return render_template('NewUser.html')
+
+
+@app.route("/NewProduct")
+def NewProduct():
+    return render_template('NewProduct.html')
+
+
+@app.route("/AdminHome")
+def AdminHome():
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM regtb  ")
+    data = cur.fetchall()
+    return render_template('AdminHome.html', data=data)
+
+
+@app.route("/ProductInfo")
+def AProductInfo():
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM protb  ")
+    data = cur.fetchall()
+    return render_template('ProductInfo.html', data=data)
+
+
+@app.route("/adminlogin", methods=['GET', 'POST'])
+def adminlogin():
+    if request.method == 'POST':
+        if request.form['uname'] == 'nilash' and request.form['password'] == 'rath':
+
+            conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM regtb ")
+            data = cur.fetchall()
+            flash("Login successfully")
+            return render_template('AdminHome.html', data=data)
+
+        else:
+            flash("UserName Or Password Incorrect!")
+            return render_template('AdminLogin.html')
+
+
+@app.route("/ARemove")
+def ARemove():
+    id = request.args.get('id')
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cursor = conn.cursor()
+    cursor.execute(
+        "delete from protb where id='" + id + "'")
+    conn.commit()
+    conn.close()
+
+    flash('Product  info Remove Successfully!')
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM protb  ")
+    data = cur.fetchall()
+    return render_template('ProductInfo.html', data=data)
+
+
+@app.route("/newproduct", methods=['GET', 'POST'])
+def newproduct():
+    if request.method == 'POST':
+        pname = request.form['pname']
+        ptype = request.form['ptype']
+        price = request.form['price']
+        qty = request.form['qty']
+        info = request.form['Info']
+
+        import random
+        file = request.files['file']
+        fnew = random.randint(1111, 9999)
+        savename = str(fnew) + ".png"
+        file.save("static/upload/" + savename)
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO  protb VALUES ('','" + pname + "','" + ptype + "','" + price + "','" + qty + "','" + info + "','" + savename + "')")
+        conn.commit()
+        conn.close()
+
+    flash('Product Register successfully')
+    return render_template('NewProduct.html')
+
+
+@app.route("/newuser", methods=['GET', 'POST'])
+def newuser():
+    if request.method == 'POST':
+        name = request.form['name']
+        mobile = request.form['mobile']
+
+        email = request.form['email']
+
+        address = request.form['address']
+
+        uname = request.form['uname']
+        password = request.form['password']
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO regtb VALUES ('" + name + "','" + email + "','" + mobile + "','" + address + "','" + uname + "','" + password + "')")
+        conn.commit()
+        conn.close()
+        flash('User Register successfully')
+
+    return render_template('UserLogin.html')
+
+
+@app.route("/userlogin", methods=['GET', 'POST'])
+def userlogin():
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['password']
+        session['uname'] = request.form['uname']
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * from regtb where username='" + username + "' and Password='" + password + "'")
+        data = cursor.fetchone()
+        if data is None:
+
+            flash('Username or Password is wrong')
+            return render_template('UserLogin.html')
+        else:
+
+            conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM regtb where username='" + username + "' and Password='" + password + "'")
+            data = cur.fetchall()
+            flash("Login successfully")
+
+            return render_template('UserHome.html', data=data)
+
+
+@app.route("/Search")
+def Search():
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM protb ")
+    data = cur.fetchall()
+
+    return render_template('Search.html', data=data)
+
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        ptype = request.form['ptype']
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM protb where  ProductType ='" + ptype + "'")
+        data = cur.fetchall()
+
+        return render_template('Search.html', data=data)
+
+
+@app.route("/UserHome")
+def UserHome():
+    uname = session['uname']
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    # cursor = conn.cursor()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  regtb where username='" + uname + "'  ")
+    data = cur.fetchall()
+
+    return render_template('UserHome.html', data=data)
+
+
+@app.route("/Add")
+def Add():
+    id = request.args.get('id')
+    session['pid'] = id
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM protb  where id='" + id + "' ")
+    data = cur.fetchall()
+    return render_template('AddCart.html', data=data)
+
+
+@app.route("/addcart", methods=['GET', 'POST'])
+def addcart():
+    if request.method == 'POST':
+        import datetime
+        date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+        pid = session['pid']
+        uname = session['uname']
+        qty = request.form['qty']
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cursor = conn.cursor()
+        cursor.execute("SELECT  *  FROM protb  where  id='" + pid + "'")
+        data = cursor.fetchone()
+
+        if data:
+            ProductName = data[1]
+            Producttype = data[2]
+            price = data[3]
+            cQty = data[4]
+
+            Image = data[6]
+
+        else:
+            return 'No Record Found!'
+
+        tprice = float(price) * float(qty)
+
+        clqty = float(cQty) - float(qty)
+
+        if clqty < 0:
+
+            flash('Low  Product ')
+
+            conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM protb  where id='" + pid + "' ")
+            data = cur.fetchall()
+            return render_template('AddCart.html', data=data)
+
+        else:
+            conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO carttb VALUES ('','" + uname + "','" + ProductName + "','" + Producttype + "','" + str(
+                    price) + "','" + str(qty) + "','" + str(tprice) + "','" +
+                Image + "','" + date + "','0','')")
+            conn.commit()
+            conn.close()
+
+            flash('Add To Cart  Successfully')
+            conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM protb  where id='" + pid + "' ")
+            data = cur.fetchall()
+            return render_template('AddCart.html', data=data)
+
+
+@app.route("/Cart")
+def Cart():
+    uname = session['uname']
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  carttb where UserName='" + uname + "' and Status='0' ")
+    data = cur.fetchall()
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT  sum(Qty) as qty ,sum(Tprice) as Tprice   FROM  carttb where UserName='" + uname + "' and Status='0' ")
+    data1 = cursor.fetchone()
+    if data1:
+        tqty = data1[0]
+        tprice = data1[1]
+    else:
+        return 'No Record Found!'
+
+    return render_template('Cart.html', data=data, tqty=tqty, tprice=tprice)
+
+
+@app.route("/RemoveCart")
+def RemoveCart():
+    id = request.args.get('id')
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cursor = conn.cursor()
+    cursor.execute(
+        "delete from carttb where id='" + id + "'")
+    conn.commit()
+    conn.close()
+
+    flash('Product Remove Successfully!')
+
+    uname = session['uname']
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  carttb where UserName='" + uname + "' and Status='0' ")
+    data = cur.fetchall()
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT  sum(Qty) as qty ,sum(Tprice) as Tprice   FROM  carttb where UserName='" + uname + "' and Status='0' ")
+    data1 = cursor.fetchone()
+    if data1:
+        tqty = data1[0]
+        tprice = data1[1]
+
+    return render_template('Cart.html', data=data, tqty=tqty, tprice=tprice)
+
+
+@app.route("/payment", methods=['GET', 'POST'])
+def payment():
+    if request.method == 'POST':
+        import datetime
+        date = datetime.datetime.now().strftime('%Y-%m-%d')
+        uname = session['uname']
+        cname = request.form['cname']
+        Cardno = request.form['cno']
+        Cvno = request.form['cvno']
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT  sum(Qty) as qty ,sum(Tprice) as Tprice   FROM  carttb where UserName='" + uname + "' and Status='0' ")
+        data1 = cursor.fetchone()
+        if data1:
+            tqty = data1[0]
+            tprice = data1[1]
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT  count(*) As count  FROM booktb ")
+        data = cursor.fetchone()
+        if data:
+            bookno = data[0]
+            print(bookno)
+
+            if bookno == 'Null' or bookno == 0:
+                bookno = 1
+            else:
+                bookno += 1
+
+        else:
+            return 'Incorrect username / password !'
+
+        bookno = 'BOOKID' + str(bookno)
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cursor = conn.cursor()
+        cursor.execute(
+            "update   carttb set status='1',Bookid='" + bookno + "' where UserName='" + uname + "' and Status='0' ")
+        conn.commit()
+        conn.close()
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO booktb VALUES ('','" + uname + "','" + bookno + "','" + str(tqty) + "','" + str(
+                tprice) + "','" + cname + "','" + Cardno + "','" + Cvno + "','" + date + "')")
+        conn.commit()
+        conn.close()
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM  carttb where UserName='" + uname + "' and Status='1' ")
+        data1 = cur.fetchall()
+
+        conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM  booktb where username='" + uname + "'")
+        data2 = cur.fetchall()
+
+    return render_template('UserBook.html', data1=data1, data2=data2)
+
+
+@app.route("/BookInfo")
+def BookInfo():
+    uname = session['uname']
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  carttb where UserName='" + uname + "' and Status='1' ")
+    data1 = cur.fetchall()
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  booktb where username='" + uname + "'")
+    data2 = cur.fetchall()
+
+    return render_template('UserBook.html', data1=data1, data2=data2)
+
+
+@app.route("/ASalesInfo")
+def ASalesInfo():
+    uname = session['uname']
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  carttb where UserName='" + uname + "' and Status='1' ")
+    data1 = cur.fetchall()
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  booktb where username='" + uname + "'")
+    data2 = cur.fetchall()
+
+    return render_template('ASalesInfo.html', data1=data1, data2=data2)
+
+
+@app.route("/ESalesInfo")
+def ESalesInfo():
+    uname = session['uname']
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  carttb where UserName='" + uname + "' and Status='1' ")
+    data1 = cur.fetchall()
+
+    conn = mysql.connector.connect(user='root', password='', host='localhost', database='1caraceespy')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM  booktb where username='" + uname + "'")
+    data2 = cur.fetchall()
+
+    return render_template('ESalesInfo.html', data1=data1, data2=data2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
