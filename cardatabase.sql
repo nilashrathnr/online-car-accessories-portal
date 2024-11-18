23497098811af773278d4bb88a2bb07109834645
NILASH RATH J <148995142+nilashrathnr@users.noreply.github.com>
2024-11-18 12:15:09 +0530

    Add files via upload

---
+++ cardatabase.sql
@@ -0,0 +1,59 @@
+
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+CREATE TABLE `booktb` (
+  `id` bigint(10) NOT NULL auto_increment,
+  `UserName` varchar(250) NOT NULL,
+  `Bookid` varchar(250) NOT NULL,
+  `Qty` varchar(250) NOT NULL,
+  `Amount` varchar(250) NOT NULL,
+  `CardName` varchar(250) NOT NULL,
+  `CardNo` varchar(250) NOT NULL,
+  `CvNo` varchar(250) NOT NULL,
+  `Date` date NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- Table structure for table `carttb`
+
+CREATE TABLE `carttb` (
+  `id` bigint(10) NOT NULL auto_increment,
+  `UserName` varchar(250) NOT NULL,
+  `ProductName` varchar(250) NOT NULL,
+  `ProductType` varchar(250) NOT NULL,
+  `Price` varchar(250) NOT NULL,
+  `Qty` decimal(18,2) NOT NULL,
+  `Tprice` decimal(28,2) NOT NULL,
+  `Image` varchar(500) NOT NULL,
+  `Date` date NOT NULL,
+  `Status` varchar(250) NOT NULL,
+  `Bookid` varchar(250) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- Table structure for table `protb`
+
+CREATE TABLE `protb` (
+  `id` bigint(10) NOT NULL auto_increment,
+  `ProductName` varchar(250) NOT NULL,
+  `ProductType` varchar(250) NOT NULL,
+  `Price` varchar(250) NOT NULL,
+  `Qty` varchar(250) NOT NULL,
+  `Info` varchar(500) NOT NULL,
+  `Image` varchar(500) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+-- Table structure for table `regtb`
+
+CREATE TABLE `regtb` (
+  `Name` varchar(250) NOT NULL,
+  `Email` varchar(250) NOT NULL,
+  `Mobile` varchar(250) NOT NULL,
+  `Address` varchar(250) NOT NULL,
+  `UserName` varchar(250) NOT NULL,
+  `Password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
