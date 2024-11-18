865558d8bd65dc9ada8e0b52773ce5faa0a96a45
NILASH RATH J <148995142+nilashrathnr@users.noreply.github.com>
2024-11-18 10:36:58 +0530

    Update cardatabase.sql

--- cardatabase.sql
+++ cardatabase.sql
@@ -1,27 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 2.11.6
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 26, 2023 at 04:57 AM
--- Server version: 5.0.51
--- PHP Version: 5.2.6
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
---
--- Database: `1herbsdbpy`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `booktb`
 --
 
@@ -38,14 +18,7 @@ CREATE TABLE `booktb` (
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `booktb`
---
 
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `carttb`
 --
 
@@ -64,14 +37,7 @@ CREATE TABLE `carttb` (
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `carttb`
---
 
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `protb`
 --
 
@@ -86,18 +52,7 @@ CREATE TABLE `protb` (
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
---
--- Dumping data for table `protb`
---
 
--- INSERT INTO `protb` (`id`, `ProductName`, `ProductType`, `Price`, `Qty`, `Info`, `Image`) VALUES
--- (3, 'HealthBen', 'ImmuneSystem', '900', '80', 'fdh', '9391.png');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `regtb`
---
 
 CREATE TABLE `regtb` (
   `Name` varchar(250) NOT NULL,
@@ -108,10 +63,4 @@ CREATE TABLE `regtb` (
   `Password` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `regtb`
---
 
--- INSERT INTO `regtb` (`Name`, `Email`, `Mobile`, `Address`, `UserName`, `Password`) VALUES
--- ('san', 'sangeeth5535@gmail.com', '9486365535', 'No 16, Samnath Plaza, Madurai Main Road, Melapudhur', 'san', 'san'),
--- ('sangeeth Kumar', 'sangeeth5535@gmail.com', '9486365535', 'No 16, Samnath Plaza, Madurai Main Road, Melapudhur', 'san', 'San');
